fb6650ebc9674a3438ea3b93b5e9378ad918b125
yusuf <siddiquiy883@gmail.com>
2026-07-21 13:39:39 +0400

    Fix GTM not loading: use dangerouslySetInnerHTML instead of Next.js Script component in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {/* Google Tag Manager - inline script for immediate loading */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PB9TB9GM');`}
-        </Script>
-        {/* End Google Tag Manager */}
+})(window,document,'script','dataLayer','GTM-PB9TB9GM');`,
+          }}
+        />
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
